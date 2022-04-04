91e071c6bacf7e501a060cb7ff4e995090390de6
ilGhera <andrea@ilghera.com>
2022-04-04 17:58:51 +0200

    Eliminazione codice ridondante global WC

--- includes/class-18app-gateway.php
+++ includes/class-18app-gateway.php
@@ -265,8 +265,6 @@ class WC18_18app_Gateway extends WC_Payment_Gateway {
 
         global $woocommerce;
 
-        global $woocommerce;
-
         $output     = 1; 
         $order      = wc_get_order( $order_id );
         $soapClient = new wc18_soap_client( $code_18app, $import );
